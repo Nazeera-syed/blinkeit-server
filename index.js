9fa1489c4af56a63bac4c777f690cbde1c3f93f5
Nazeera-syed <syednazeera730@gmail.com>
2025-11-06 22:49:48 +0530

    Update CORS settings to allow Vercel frontend and localhost

--- index.js
+++ index.js
@@ -29,10 +29,24 @@ app.post(
 );
 
 
-app.use(cors({
-    credentials:true,
-    origin:process.env.FRONTEND_URL
-}))
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://blinkeit.vercel.app"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log("❌ Blocked by CORS:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
